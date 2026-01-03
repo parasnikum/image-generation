c669bcf0c3b01cdd4050c1a8203c9fe21766df51
Paras <parasnikum04@gmail.com>
2026-01-03 23:50:18 +0530

    Fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizeCss: false
-  }
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+export default nextConfig;
